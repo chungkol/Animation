32e52c3bb564c10743bc30fd15af7fab5b869172
chung <chungvtgc00989@fpt.edu.vn>
2016-08-23 10:15:46 +0700

    update

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -9,69 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let duration:NSTimeInterval = 2.0
+    var duration:NSTimeInterval = 2.0
+    
+    @IBOutlet weak var lbName: UILabel!
+    
     @IBOutlet weak var logo: UIImageView!
-   
-    @IBAction func btnMove(sender: UIButton) {
+    
+    @IBOutlet weak var lbZing: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.logo!.alpha = 0
+        self.lbName!.alpha = 0
+        self.lbZing!.alpha = 0
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
         UIView.animateWithDuration(duration, animations: {
-             self.logo.center = CGPointMake(230, self.logo!.center.y)
+            self.logo!.alpha = 1
+
         }) { (finished) in
-        UIView.animateWithDuration(self.duration, delay: 0.0, options: UIViewAnimationOptions.CurveLinear, animations: {
-            self.logo.transform = CGAffineTransformIdentity
-            self.logo.frame.origin.x = 30
-            }, completion: nil)
+            UIView.animateWithDuration(self.duration, animations: {
+                self.lbZing.center = CGPoint(x: self.lbZing!.center.x, y: 80)
+                self.lbZing!.alpha = 1
+            }) { (finished) in
+                UIView.animateWithDuration(self.duration, animations: {
+                    self.lbName.center = CGPoint(x: self.lbName!.center.x, y: 500)
+                    self.lbName!.alpha = 1
+                })
+                
+                
+            }
+            
+            
         }
     }
     
-
-    @IBAction func btnRotate(sender: UIButton) {
-        UIView.animateWithDuration(duration, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
-            self.logo.transform = CGAffineTransformMakeRotation(CGFloat(M_PI_2))
-            }, completion: nil)
-    }
-    
-    @IBAction func btnFlip(sender: UIButton) {
-        UIView.animateWithDuration(duration, delay: 0.0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
-            self.logo.transform = CGAffineTransformMakeScale(-1, 1)
-            }, completion: nil)
-    }
-    
-    @IBAction func btnAlpha(sender: UIButton) {
-        self.logo.alpha = 0
-        UIView.animateWithDuration(duration, delay: 0.0, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
-            self.logo.alpha = 1
-            }, completion: nil)
-    }
-    
-    @IBAction func btnScale(sender: UIButton) {
-        UIView.animateWithDuration(duration, delay: 0.0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
-           self.logo.transform = CGAffineTransformMakeScale(2, 2)
-            }, completion: nil)
-    }
-    
-    @IBAction func btnMix(sender: UIButton) {
-        self.logo.alpha = 0
-        let t1:CGAffineTransform = CGAffineTransformMakeRotation(CGFloat(M_PI_2))
-        let t2:CGAffineTransform = CGAffineTransformMakeScale(2, 2)
-        UIView.animateWithDuration(duration, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
-            self.logo.alpha = 1
-            self.logo.frame.origin.x = 230
-            self.logo.transform = CGAffineTransformConcat(t1, t2)
-            }, completion: nil)
-    }
-    
-    @IBAction func btnReset(sender: UIButton) {
-        self.logo.transform = CGAffineTransformIdentity
-        self.logo.frame.origin.x = 30
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
-    }
     
     
-
-
+    
 }
 
